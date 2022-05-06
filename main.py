63aa6f19af8755251145b1f417e90330608b1a65
00Ali00 <103453597+00Ali00@users.noreply.github.com>
2022-05-06 23:42:19 +0300

    f

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 import pygame
+
 WIDTH = 600
 HEIGHT = 600
 FPS = 60
 TILESIZE = 32
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+class Player(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
 
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((HEIGHT, WIDTH))
 pygame.display.set_caption('RPG')
 clock = pygame.time.Clock()
+
 all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 fl_Start = True
 while fl_Start:
@@ -21,3 +45,4 @@ while fl_Start:
     all_sprites.update()
     screen.fill('black')
     all_sprites.draw(screen)
+    pygame.display.flip()
