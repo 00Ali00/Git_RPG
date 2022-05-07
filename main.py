b37421d13afb9278b82e73ddc7a38d8f2c93056a
00Ali00 <103453597+00Ali00@users.noreply.github.com>
2022-05-07 22:12:26 +0300

    ds

--- main.py
+++ main.py
@@ -1,25 +1,71 @@
-import pygame
+import pygame as pg
+import sys
 from settings import *
-from player import Player
+from sprites import *
 
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((HEIGHT, WIDTH))
-pygame.display.set_caption('RPG')
-clock = pygame.time.Clock()
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.load_data()
 
-all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
+    def load_data(self):
+        pass
 
-fl_Start = True
-while fl_Start:
-    delta_time = clock.tick(FPS) / 1000
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            fl_Start = False
+    def new(self):
+        # initialize all variables and do all the setup for a new game
+        self.all_sprites = pg.sprite.Group()
 
-    all_sprites.update(delta_time)
-    screen.fill('black')
-    all_sprites.draw(screen)
-    pygame.display.flip()
+    def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+    def update(self):
+        # update portion of the game loop
+        self.all_sprites.update()
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+# create the game object
+game = Game()
+game.show_start_screen()
+while True:
+    game.new()
+    game.run()
+    game.show_go_screen()
--- settings.py
+++ settings.py
@@ -1,12 +1,18 @@
-import pygame
-
-WIDTH = 600
-HEIGHT = 600
-FPS = 60
-TILESIZE = 32
-
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
 GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# game settings
+WIDTH = 1024   # 16 * 64 or 32 * 32 or 64 * 16 max:1366
+HEIGHT = 640  # 16 * 48 or 32 * 24 or 64 * 12 max:768
+FPS = 60
+TITLE = "Tilemap Demo"
+BGCOLOR = DARKGREY
+
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
