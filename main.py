fbeed7f1fdb2c89dd563c59ab89244eed2215620
00Ali00 <103453597+00Ali00@users.noreply.github.com>
2022-05-07 19:15:18 +0300

    f

--- main.py
+++ main.py
@@ -19,10 +19,6 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
 
-    def update(self):
-        self.rect.x += 3
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
 
 pygame.init()
 pygame.mixer.init()
